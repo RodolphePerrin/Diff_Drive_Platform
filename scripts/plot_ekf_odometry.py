ffce695fada3b87dbda6f4ab6fda19ce5649a487
Rodolphe Perrin <rodolpheperrin@MacBook-Air-de-Rodolphe.local>
2020-05-24 13:25:42 -0400

    First commit

---
+++ scripts/plot_ekf_odometry.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+from matplotlib import pyplot as pp
+
+
+def load_odometry(path):
+    odometry = []
+
+    lines = []
+    for line in open(path):
+        reading = line.split()
+        odometry.append(reading)
+
+    return odometry
+
+
+def plot_odometry(path_encoders, path_ground_truth, path_filtered):
+    
+    encoders = load_odometry(path_encoders)
+    (t, x_encoders, y_encoders,vx, vy) = zip(*encoders)
+    
+    ground_truth = load_odometry(path_ground_truth)
+    (t, x_truth, y_truth, vx, vy) = zip(*ground_truth)
+    filtered = load_odometry(path_filtered)
+    print(filtered)
+    (t, x_filtered, y_filtered, vx, vy) = zip(*filtered)
+
+    pp.plot(x_encoders, y_encoders, 'b.-')
+    pp.plot(x_filtered, y_filtered, 'r,-')
+    pp.plot(x_truth, y_truth, 'g.-')
+    pp.show()
+
+
+def main():
+    from sys import argv
+    plot_odometry(*argv[1:])
+
+
+if __name__ == '__main__':
+    main()
+
